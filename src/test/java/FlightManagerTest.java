ac4302ad019e9e5625cddec2e2907d146e7709d8
Nelson <nbritob@hotmail.com>
2020-01-10 17:25:02 +0000

    fixes the weight per passenger I think, will see if something goes wrong later, its a 50/50 either it does or it doesnt

--- src/test/java/FlightManagerTest.java
+++ src/test/java/FlightManagerTest.java
@@ -13,13 +13,17 @@ public class FlightManagerTest {
     @Before
     public void before () {
         plane = new Plane(PlaneType.BOEING212);
-        passenger = new Passenger("Dan", 2);
+        passenger = new Passenger("Dan", 1);
         flight = new Flight(plane, "L33T", "Metropolis", "Gotham", "09:00");
         flightManager = new FlightManager(flight, passenger);
     }
 
     @Test
     public void shouldCalculateWeightAllowedPerPassenger() {
+        flight.addPassenger(passenger);
+        flight.addPassenger(passenger);
+        flight.addPassenger(passenger);
+        flight.addPassenger(passenger);
         flight.addPassenger(passenger);
         assertEquals(8000, flightManager.getWeightPerPassenger());
     }
