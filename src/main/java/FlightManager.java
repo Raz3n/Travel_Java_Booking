24bc31e0d6eb9ecd00f79325a1e2440b11d99bba
Nelson <nbritob@hotmail.com>
2020-01-10 16:59:24 +0000

    FlightManager setup done

--- src/main/java/FlightManager.java
+++ src/main/java/FlightManager.java
@@ -1,2 +1,9 @@
 public class FlightManager {
+    private Flight flight;
+    private Passenger passenger;
+
+    public FlightManager(Flight flight, Passenger passenger) {
+        this.flight = flight;
+        this.passenger = passenger;
+    }
 }
--- src/test/java/FlightManagerTest.java
+++ src/test/java/FlightManagerTest.java
@@ -1,3 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class FlightManagerTest {
 
+    private FlightManager flightManager;
+    private Passenger passenger;
+    private Plane plane;
+    private Flight flight;
+
+    @Before
+    public void before () {
+        plane = new Plane(PlaneType.BOEING212);
+        passenger = new Passenger("Dan", 2);
+        flight = new Flight(plane, "L33T", "Metropolis", "Gotham", "09:00");
+        flightManager = new FlightManager();
+    }
+
+    @Test
+    public void
 }
