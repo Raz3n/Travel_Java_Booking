9ad5882213247373132d4ac6aa52366c77083069
Nelson <nbritob@hotmail.com>
2020-01-10 13:13:57 +0000

    Flight set up done

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Flight {
+
+    private Plane plane;
+    private ArrayList<Passenger> passengers;
+    private String flightNumber;
+    private String destination;
+    private String departureAirport;
+    private String departureTime;
+
+    public Flight(Plane plane, ArrayList<Passenger> passengers, String flightNumber, String destination, String departureAirport, String departureTime) {
+        this.plane = plane;
+        this.passengers = passengers;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class FlightTest {
+
+    private Flight flight;
+    private Plane plane;
+    private ArrayList<Passenger> passengers;
+    private Passenger passenger1;
+    private Passenger passenger2;
+    private Passenger passenger3;
+    private Passenger passenger4;
+
 }
