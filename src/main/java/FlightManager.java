8fab6cf71ce41f15b7218b92e7370e3d09b84ff1
Nelson <nbritob@hotmail.com>
2020-01-12 16:03:25 +0000

    some extension done

--- src/main/java/FlightManager.java
+++ src/main/java/FlightManager.java
@@ -8,6 +8,7 @@ public class FlightManager {
     }
 
     public int getWeightPerPassenger() {
+        //math for me 80000 / 2 = 40000 / passengers (bags 5) = 8000;
         return (flight.getPlane().getWeight() / 2) / flight.getPassengers().size();
     }
 }
